ed0b894d157ff581b776b92910b4d11a5a461fe9
fang0rnz <lucas.sartm@outlook.com>
2016-10-14 09:54:08 -0300

    APANHANDO DE REGEX

--- src/eleicao/Teste.java
+++ src/eleicao/Teste.java
@@ -2,14 +2,16 @@ package eleicao;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.Scanner;
-
+import java.util.Locale;
 public class Teste {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException, ParseException {
 		File file = new File("/home/2014100667/workspace1/trabalhoprog3/src/eleicao/vitoria2016.csv");
 		Scanner sc = new Scanner(file);
-		sc.useDelimiter(";");
+		sc.useDelimiter(";|\\n");
 		sc.nextLine();
 		String pos = sc.next();
 		int posic = 0;
@@ -22,7 +24,11 @@ public class Teste {
 		System.out.println(sc.next());
 		System.out.println(sc.next());
 		System.out.println(sc.next());
-		
+		NumberFormat nf = NumberFormat.getNumberInstance(); 
+		nf.setGroupingUsed(true);
+		Number teste = nf.parse(sc.next());
+		//System.out.println(teste);
+		System.out.println(sc.next());
 		
 		
 
