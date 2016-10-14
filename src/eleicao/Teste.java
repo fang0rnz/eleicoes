3084e7561fbf7e3a325a9619b969f2f16edee241
fang0rnz <lucas.sartm@outlook.com>
2016-10-14 10:20:23 -0300

    só os loucos sabem

--- src/eleicao/Teste.java
+++ src/eleicao/Teste.java
@@ -15,22 +15,27 @@ public class Teste {
 		sc.nextLine();
 		String pos = sc.next();
 		int posic = 0;
-		if (pos.contains("*")){
-			System.out.println("Candidato Eleito.");
+		if (pos.contains("*")||pos.contains("#")){
+			if (pos.charAt(0) == '*')
+				System.out.println("Candidato eleito.");
+			else
+				System.out.println("Candidato desclassificado");
 			pos = pos.substring(1);
-			posic = Integer.parseInt(pos);
 		}
+			posic = Integer.parseInt(pos);
 		System.out.println(posic);
-		System.out.println(sc.next());
-		System.out.println(sc.next());
-		System.out.println(sc.next());
+		int numerocand = Integer.parseInt(sc.next());
+		System.out.println(numerocand);
+		String nome = sc.next();
+		System.out.println(nome);
+		String partidocolig = sc.next();
+		System.out.println(partidocolig);
 		NumberFormat nf = NumberFormat.getNumberInstance(); 
 		nf.setGroupingUsed(true);
 		String teste = sc.next();
-		Number teste1 = (nf.parse(teste));
-		int teste2 = teste1.intValue();
+		int teste1 = (nf.parse(teste)).intValue(); //inteiro do valor de votos do candidato
 		System.out.println(teste);
-		System.out.println(teste2);
+		System.out.println(teste1);
 		
 		System.out.println(sc.next());
 		
