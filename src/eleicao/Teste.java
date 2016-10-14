d7dd6288f48797c72bc968ee6640aed42a833975
fang0rnz <lucas.sartm@outlook.com>
2016-10-14 13:33:58 -0300

    Acertado método de captura e tratamento dos dados

--- src/eleicao/Teste.java
+++ src/eleicao/Teste.java
@@ -12,9 +12,10 @@ public class Teste {
 		File file = new File("/home/2014100667/workspace1/trabalhoprog3/src/eleicao/vitoria2016.csv");
 		Scanner sc = new Scanner(file);
 		sc.useDelimiter(";|\\n");
-		sc.nextLine();
+		String atual = sc.nextLine();
+		Scanner scanline = new Scanner (atual);
+		scanline.useDelimiter(";|\\n");
 		String pos = sc.next();
-		int posic = 0;
 		if (pos.contains("*")||pos.contains("#")){
 			if (pos.charAt(0) == '*')
 				System.out.println("Candidato eleito.");
@@ -22,22 +23,25 @@ public class Teste {
 				System.out.println("Candidato desclassificado");
 			pos = pos.substring(1);
 		}
-			posic = Integer.parseInt(pos);
+			int posic = Integer.parseInt(pos);
 		System.out.println(posic);
 		int numerocand = Integer.parseInt(sc.next());
 		System.out.println(numerocand);
 		String nome = sc.next();
 		System.out.println(nome);
-		String partidocolig = sc.next();
+		sc.useDelimiter(" - |;");
+		String partido = sc.next();
+		System.out.println(partido);
+		sc.useDelimiter(";|\\n");
+		String partidocolig = sc.next().substring(3);
 		System.out.println(partidocolig);
-		NumberFormat nf = NumberFormat.getNumberInstance(); 
-		nf.setGroupingUsed(true);
-		String teste = sc.next();
-		int teste1 = (nf.parse(teste)).intValue(); //inteiro do valor de votos do candidato
-		System.out.println(teste);
-		System.out.println(teste1);
-		
-		System.out.println(sc.next());
+		NumberFormat nf = NumberFormat.getInstance(Locale.forLanguageTag("pt-br")); 
+		String votosString = sc.next();
+		int votos = (nf.parse(votosString)).intValue(); //inteiro do valor de votos do candidato
+		System.out.println(votos);
+		double pVotosValidos = (nf.parse(sc.next()).doubleValue());
+		System.out.println(pVotosValidos);
+		//System.out.println(sc.next());
 		
 		
 
