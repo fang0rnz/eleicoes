b98472715f4caf1a5e26f9cf2b31be91e7ed2780
fang0rnz <lucas.sarm@outlook.com>
2016-10-15 15:28:48 -0300

    Adicionado construtor e modelo da classe Leitor.

--- src/eleicao/Leitor.java
+++ src/eleicao/Leitor.java
@@ -1,6 +1,22 @@
 package eleicao;
 
-public abstract class Leitor {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Leitor {
+	Scanner sc = null;
+	File file = null;
+	
+	public Leitor (String path) {
+		file = new File(path);
+		try {
+			sc = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			System.out.println("Arquivo não encontrado.");
+			e.printStackTrace();
+		}
+	}
 	
 
 }
